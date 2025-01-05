78a92bb81587b3eccbde014f11065c32e2495754
JoaoBianco <joaops.bianco@gmail.com>
2025-01-05 18:02:48 -0300

    feat: add pagination to list all games

--- src/routes/gamesRawg.ts
+++ src/routes/gamesRawg.ts
@@ -1,31 +1,35 @@
-import express, { Request, Response } from 'express'
-import { RawgGame, RawgGamesList } from '../models/rawg.model'
-import { RAWGKEY, RAWGURL } from '../util/globalVariables'
+import express, { Request, Response } from "express"
+import { RawgGame, RawgGamesList } from "../models/rawg.model"
+import { RAWGKEY, RAWGURL } from "../util/globalVariables"
 
 const router = express.Router()
 
-router.get('', async (req: Request, res: Response) => {
-  const url = RAWGURL + 'games' + RAWGKEY
+router.get("", async (req: Request, res: Response) => {
+  const page = req.query.page || 1
+  const url = RAWGURL + "games" + RAWGKEY + `&page=${page}`
   await fetch(url)
-    .then(response => response.json())
+    .then((response) => response.json())
     .then((data: RawgGamesList) => {
       if (!data.results.length) {
         return res.status(404).send(data)
       }
       return res.send(data)
     })
-    .catch(err => {res.status(500).send(err); console.log(err)})
+    .catch((err) => {
+      res.status(500).send(err)
+      console.log(err)
+    })
 })
 
-router.get('/:idSlug', async (req: Request, res: Response) => {
+router.get("/:idSlug", async (req: Request, res: Response) => {
   try {
     const dirtyGame = await fetchRawgGame(req.params.idSlug)
     if (!dirtyGame) {
-      res.status(404).send({ message: 'Game not found!' })
+      res.status(404).send({ message: "Game not found!" })
     }
 
-    if(dirtyGame?.detail) {
-      res.status(404).send({ message: 'Game not found!' })
+    if (dirtyGame?.detail) {
+      res.status(404).send({ message: "Game not found!" })
       return
     }
 
@@ -35,17 +39,19 @@ router.get('/:idSlug', async (req: Request, res: Response) => {
     return
   } catch (error) {
     console.log(error)
-    res.status(500).send({ message: 'Internal server error' })
+    res.status(500).send({ message: "Internal server error" })
     return
   }
 })
 
 async function fetchRawgGame(idOrSlug: string): Promise<RawgGame> {
-  const url = RAWGURL + 'games/' + idOrSlug + RAWGKEY
-  return await fetch(url).then(response => response.json())
+  const url = RAWGURL + "games/" + idOrSlug + RAWGKEY
+  return await fetch(url).then((response) => response.json())
 }
 
-async function verifyIfRedirect(game: RawgGame | { redirect: boolean, slug: string }) {
+async function verifyIfRedirect(
+  game: RawgGame | { redirect: boolean; slug: string }
+) {
   if (game?.redirect) {
     return await fetchRawgGame(game.slug)
   }
@@ -53,4 +59,3 @@ async function verifyIfRedirect(game: RawgGame | { redirect: boolean, slug: stri
 }
 
 export { router as gamesRawgRouter }
-
